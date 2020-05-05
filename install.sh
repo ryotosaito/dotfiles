0a9c3db1465e04026c286f891c06809f0b02354e
Ryoto Saito <ryoto.no.address@gmail.com>
2020-05-06 01:55:19 +0900

    update on 2020/05/06 01:55:19

--- install.sh
+++ install.sh
@@ -1,5 +1,46 @@
 #!/usr/bin/env bash
 
+print_help() {
+	cat <<-EOS
+	$0: install dotfiles to your home directory
+
+	OPTION:
+	  -h, --help: print help
+	  -i, --init: initial install (install VundleVim and Plugins)
+	EOS
+}
+
+while getopts hi-: OPT
+do
+	case $OPT in
+		h)
+			print_help
+			exit
+			;;
+		i)
+			INIT_FLAG=1
+			;;
+		-)
+			case $OPTARG in
+				help)
+					print_help
+					exit
+					;;
+				init)
+					INIT_FLAG=1
+					;;
+			esac
+			;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+[[ -v INIT_FLAG ]] && {
+	mkdir -p "$HOME/.vim/colors"
+	wget -P "$HOME/.vim/colors/" https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+	vim +PluginInstall +qall
+}
+
 REPODIR="$(cd $(dirname $0); pwd)"
 cd $REPODIR
 
