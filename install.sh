a69b7d3616603d3f7545ba726abf079f7fc7ebd8
Ryoto Saito <ryoto.no.address@gmail.com>
2020-05-06 02:09:21 +0900

    add .screenrc

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ cd $REPODIR
 
 echo "DOTFILES=\"$REPODIR\"" > .dotfiles_path
 
-for file in .bash_logout .bash_profile .bashrc .dotfiles_path .profile .rc .vimrc
+for file in .bash_logout .bash_profile .bashrc .dotfiles_path .profile .rc .screenrc .vimrc
 do
 	ln -is "$REPODIR/$file" "$HOME/$file"
 done
